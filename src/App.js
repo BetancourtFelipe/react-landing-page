ac711fc3d5945e02ed8f8b02f40ac53886cc4ce5
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2023-01-23 21:02:53 +0100

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+/** @jsxImportSource @emotion/react */
 import './App.css';
+import { css } from '@emotion/react';
+
+const noticeSectionStyle = css`
+  max-width: 1080px;
+  margin: 1em auto;
+  background-color: #c7f0e5;
+  padding: 1em 2em;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  border: 1px solid #41c780;
+  width: 100%;
+
+  span {
+    color: grey;
+  }
+  a {
+    color: grey;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <section css={noticeSectionStyle}>
+        <strong>NEW</strong>
+        <span>
+          Get the latest project updates, delivered right to your inbox!
+          <a href="https://updates.verify.as/">Subscribe here →</a>
+        </span>
+      </section>
+      <header>
+        <div>
+          <div></div>
+          <nav></nav>
+        </div>
       </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
